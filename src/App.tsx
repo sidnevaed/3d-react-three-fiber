ace7f8b59428678678239d8bf0d21b390b233149
sidneva.e.d <sidneva.e.d@gmail.com>
2023-11-29 11:00:52 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './App.css';
+
+export const App = ():React.JSX.Element => {
+  return <h1>Это 3d приложение</h1>;
+};
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+const optimization = {
+  minimize: IS_PROD,
+  minimizer: [
+    new TerserPlugin({
+      extractComments: false,
+      terserOptions: {
+        ie8: false,
+        ecma: 8,
+        output: {
+          comments: false,
+          beautify: false
+        },
+        compress: {
+          drop_console: false
+        },
+        warnings: false,
+        keep_classnames: true,
+        keep_fnames: true
+      }
+    })
+  ]
+};
+
+const rules = [{
+  test: /\.(tsx?)$/,
+  use: [{
+    loader: 'ts-loader'
+  }]
+},
+{
+  test: /\.css$/i,
+  use: [{
+    loader: 'style-loader'
+  }]
+},
+{
+  test: /\.css$/i,
+  use: [{
+    loader: 'css-loader',
+    options: {
+      import: true
+    }
+  }]
+}];
+
+module.exports = {
+  devServer: {
+    hot: !IS_PROD,
+    static: './app',
+    host: '0.0.0.0'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  entry: {
+    'index': ['./src/index.tsx']
+  },
+  output: {
+    path: path.join(__dirname, './app'),
+    publicPath: '/',
+    filename: './[name].js',
+    library: 'appLibrary',
+    libraryTarget: 'umd',
+    chunkFilename: './index.js',
+    globalObject: 'this'
+  },
+  plugins: [],
+  optimization,
+  module: {
+    rules
+  },
+  performance: {
+    hints: false
+  },
+  devtool: false,
+  mode: IS_PROD ? 'production' : 'development'
+};
